84cc10f6861b165491c1eed18111f52b0768f046
unknown <tb19rpl@gmail.com>
2023-05-13 12:55:55 -0700

    menambahkan darkmode dan tambahan fitu

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -10,10 +10,44 @@ humber.addEventListener('click', function () {
 window.onscroll = function (){
     const header = document.querySelector('header');
     const fixednav = header.offsetTop;
+    const top = document.querySelector('#top');
 
     if (window.pageYOffset > fixednav){
         header.classList.add('navbar-fixed');
+        top.classList.remove('hidden');
+        top.classList.add('flex');
     }else{
         header.classList.add('navbar-fixed');
+        top.classList.remove('flex');
+        top.classList.add('hidden');
     }
 };
+
+// klik dluar humber
+window.addEventListener('click', function(e){
+    if (e.target != humber && e.target != navmenu){
+        humber.classList.remove('humber-active');
+        navmenu.classList.add('hidden');
+    }
+})
+
+// dark mode toggle
+const darktoggel = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darktoggel.addEventListener('click', function(){
+    if(darktoggel.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark'
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light'
+    }
+})
+
+// posisi toggle berdasarkan mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darktoggel.checked = 'true'
+  } else {
+    darktoggel.checked = 'false'
+}
