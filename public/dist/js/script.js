2178df1841cd5c9847ba5191ac9737987ae23c0d
unknown <tb19rpl@gmail.com>
2023-05-14 07:16:21 -0700

    menambahkan fitur send pesan

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -48,6 +48,72 @@ darktoggel.addEventListener('click', function(){
 // posisi toggle berdasarkan mode
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     darktoggel.checked = 'true'
-  } else {
+} else {
     darktoggel.checked = 'false'
 }
+
+// gotowa 
+function goWhatsapp(){
+    var nama = document.getElementById("nama").value;
+    var email = document.getElementById("email").value;
+    var pesan = document.getElementById("pesan").value;
+
+    var url = "https://wa.me/+62895606108943?text=" + "Nama : "  +  nama + "%0a" + "Email : "  +  email + "%0a" + "Pesan : " + pesan;
+
+window.open(url, "_blank").focus();
+}
+
+// validation
+// var namaerror = document.getElementById('nama-error');
+// var emailerror = document.getElementById('email-error');
+// var pesanerror = document.getElementById('pesan-error');
+// var kirimerror = document.getElementById('kirim-error');
+
+// function validatenama(){
+//     var nama = document.getElementById('nama').value;
+
+//     if (nama.length ==0 ) {
+//         namaerror.innerHTML = 'Nama is required';
+//         return false;
+//     }
+//     if (!nama.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+//         namaerror.innerHTML = 'Write full name';
+//         return false;
+//     }
+//     namaerror.innerHTML = 'valid';
+//         return true;
+// }
+// function validateemail(){
+//     var email = document.getElementById('email').value;
+
+//     if (email.length ==0 ) {
+//         emailerror.innerHTML = 'Email is required';
+//         return false;
+//     }
+//     if (!email.match(/^[A-Za-z\._\-[0-9]*[@]*[A-Za-z]*[\.][a-z]{2,4}$/)) {
+//         emailerror.innerHTML = 'email invalid'
+//         return false;
+//     }
+//     emailerror.innerHTML = 'valid';
+//         return true;
+// }
+// function validatepesan(){
+//     var pesan = document.getElementById('pesan').value;
+//     var required = 20;
+//     var left = required - pesan.length;
+
+//     if (left > 0 ) {
+//         pesanerror.innerHTML = left + 'more characters required';
+//         return false;
+//     }
+//     pesanerror.innerHTML = 'valid';
+//         return true;
+// }
+// function validateform(){
+//     if(!validatenama()||!validateemail()||!validatepesan()){
+//         kirimerror.style.display = 'block';
+//         kirimerror.innerHTML = 'plish fix error';
+//         setTimeout(function(){kirimerror.style.display='none';}, 3000);
+//         return false
+//     }
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
     extend: {
       colors: {
         primary : '#14b8a6',
+        danger : '#b91c1c',
+        valid : '##15803d',
         secondary : '#64748b',
         dark : '#0f172a',
       },
